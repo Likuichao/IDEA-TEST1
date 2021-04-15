e9ce29b829136b246c987b817a0b367439fe2938
likuichao <1973310746@qq.com>
2021-04-15 18:40:38 +0800

    修改

--- test1/src/test.java
+++ test1/src/test.java
@@ -1,8 +1,12 @@
-public class test{
+public class test {
     public static void main(String[] args) {
         System.out.println("111");
     }
-    static  void print(){
+
+    static void print() {
         System.out.println("111");
+    }
+    static void print1(){
+        System.out.println("2222");
     }
 }
